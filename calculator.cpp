d976fb3d41302630a9eedaf4f5b2142c2787d0e6
TNTeshka <pupikpn@gmail.com>
2024-09-29 19:13:46 +0300

    my first commit

--- calculator.cpp
+++ calculator.cpp
@@ -3,16 +3,12 @@
 int Calculator::Add (double a, double b)
 {
 
-    return a + b;
+    return a + 2*b;
 
 }
+//coment
 
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
-}
-
-int Calculator::Mul (double a, double b)
-{
-    return a * b + 0.5;
 }
